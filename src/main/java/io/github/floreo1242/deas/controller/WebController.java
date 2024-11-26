23f65d795277dd5d1f3f410fa9868c06b7238fc4
Jinsoo Yoon <floreo1242@gmail.com>
2024-11-26 15:42:59 +0900

    Feat: 메인페이지 생성

---
+++ src/main/java/io/github/floreo1242/deas/controller/WebController.java
@@ -0,0 +1,28 @@
+package io.github.floreo1242.deas.controller;
+
+import io.github.floreo1242.deas.service.CustomUserDetails;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/home")
+    public String home(HttpSession session) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth.getPrincipal() instanceof CustomUserDetails userDetails) {
+            session.setAttribute("id", auth.getName());
+            session.setAttribute("name", userDetails.getName());
+        }
+        return "home";
+    }
+
+    @GetMapping("/event/create")
+    public String createEvent(Model model) {
+        return "create-event";
+    }
+}
